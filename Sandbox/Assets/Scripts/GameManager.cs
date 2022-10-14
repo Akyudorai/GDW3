fd45899a6c274e711ada7caa5c01144e246dc5a2
Cody Jensen <cody.jensen1@ontariotechu.net>
2022-10-14 14:44:00 -0400

    Oops 2

--- Sandbox/Assets/Scripts/GameManager.cs
+++ Sandbox/Assets/Scripts/GameManager.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public enum GameMode {
+        Destination
+    }
+
     private static GameManager instance;
     public static GameManager GetInstance() 
     {
         return instance;
     }
+
+    public PlayerController2 PlayerRef;
+    public Transform StartPoint;
+
+    private void Start() 
+    {
+        //Initalize();
+    }
+
+    private void Initialize() 
+    {   
+        PlayerRef.transform.position = StartPoint.position;
+    }
+
+
+
+
 
 }
