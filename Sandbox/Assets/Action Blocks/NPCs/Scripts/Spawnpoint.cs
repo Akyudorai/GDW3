979e70ea40e04e937f00c554ca7e516ceb12f5f5
Nathan Moore <nathaniel.m.787@gmail.com>
2022-10-16 22:32:07 -0400

    Extra Comment

--- Sandbox/Assets/Action Blocks/NPCs/Scripts/Spawnpoint.cs
+++ Sandbox/Assets/Action Blocks/NPCs/Scripts/Spawnpoint.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Spawnpoint : MonoBehaviour
 {
-    private void OnDrawGizmos()
+    private void OnDrawGizmos() // This is a fun little trick to make the NPC spawnpoints visible in the editor but not ingame :)
     {
         Gizmos.color = Color.green;
         Gizmos.DrawCube(transform.position, Vector3.one);
